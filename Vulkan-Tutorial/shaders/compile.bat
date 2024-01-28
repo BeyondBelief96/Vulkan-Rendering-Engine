a217ca39457df74c72c616036ab0bce50d4181f1
Brandon Berisford <brandonberisford@gmail.com>
2024-01-28 13:35:52 -0600

    Drawing my first triangle in vulkan with synchronization in place as well as multiple frames in flight.

---
+++ Vulkan-Tutorial/shaders/compile.bat
@@ -0,0 +1,3 @@
+C:\VulkanSDK\1.3.275.0\Bin\glslc.exe vulk.vert -o vert.spv
+C:\VulkanSDK\1.3.275.0\Bin\glslc.exe vulk.frag -o frag.spv
+pause
---
+++ Vulkan-Tutorial/util.h
@@ -0,0 +1,22 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <fstream>
+#include <vector>
+
+std::vector<char> ReadFile(const std::string& filename)
+{
+	std::ifstream file(filename, std::ios::ate | std::ios::binary);
+	if (!file.is_open())
+		throw std::runtime_error("Failed to open file!");
+
+	const size_t fileSize = static_cast<size_t>(file.tellg());
+	std::vector<char> buffer(fileSize);
+	file.seekg(0);
+	file.read(buffer.data(), fileSize);
+	file.close();
+	return buffer;
+
+}
+
+#endif
